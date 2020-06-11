83551b7a7658893cb41535f378463f9509127015
Takana-Norimasa <neuromancer_wg@outlook.jp>
2020-06-11 14:47:18 +0900

    change grid format 2,3 -> c,3

--- src/display.c
+++ src/display.c
@@ -3,37 +3,38 @@
 void display_block(Block *block){
 	switch(block->state){
 		case MINE:
-			printf("  #");
+			printf(" #");
 			break;
 		case HINT:
-			printf("%3d",block->mine_num);
+			printf("%d",block->mine_num);
 			break;
 		case NONE:
-			printf("   ");
+			printf("  ");
 			break;
 	}
 }
 
 void display_field(){
 	int i,j;
+	char c;
 
 	//printf("\033c");
 	printf("\033[1;1H");
 	printf("\033[2J");
 	
 
-	printf("    ");
-	for(i=0;i<field.size;i++)
-		printf("%3d",i);
+	printf("   ");
+	for(c='a';c<'a'+field.size;c++)
+		printf(" %c",c);
 	printf("\n");
 
-	printf("    ");
+	printf("   ");
 	for(i=0;i<field.size;i++)
-		printf("---",i);
+		printf("--",i);
 	printf("\n");
 
 	for(i=0;i<field.size;i++){
-		printf("%3d|",i);
+		printf("%2d|",i);
 		for(j=0;j<field.size;j++)
 			display_block(&(field.matrix[i][j]));
 		printf("\n");
